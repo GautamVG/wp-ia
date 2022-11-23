b2bb11096a289adb76d6ead1a8be96ad9a3b8ddf
Gautam <gautam.gv@somaiya.edu>
2022-11-24 01:28:18 +0530

    Changed password of admin

--- db/populate.sql
+++ db/populate.sql
@@ -37,44 +37,44 @@ INSERT INTO `user` (
     (
         'Administrator',
         'admin',
-        'hellokitty',
+        '04dac8afe0ca501587bad66f6b5ce5ad',
         1
     ),
     -- Ground Managers
-    (
-        'Abhinandan Goswami',
-        'abhi.goswami',
-        'gogosami',
-        2
-    ),
-    (
-        'Pramod Dubey',
-        'pramod.dubey',
-        'dubeyji',
-        2
-    ),
+    -- (
+    --     'Abhinandan Goswami',
+    --     'abhi.goswami',
+    --     'gogosami',
+    --     2
+    -- ),
+    -- (
+    --     'Pramod Dubey',
+    --     'pramod.dubey',
+    --     'dubeyji',
+    --     2
+    -- ),
     -- Students
-    (
-        'Gautam Vishwakarma',
-        'gautam.gv',
-        'hellokitty',
-        3
-    ),
-    (
-        'Aditya Pai',
-        'aditya.pai',
-        'hailjesus',
-        3
-    ),
-    (
-        'Chintan Shukla',
-        'c.shukla',
-        'purplelove',
-        3
-    ),
-    (
-        'Harshal Dave',
-        'harshal.dave',
-        'roadyahinbanega',
-        3
-    );
+    -- (
+    --     'Gautam Vishwakarma',
+    --     'gautam.gv',
+    --     'hellokitty',
+    --     3
+    -- ),
+    -- (
+    --     'Aditya Pai',
+    --     'aditya.pai',
+    --     'hailjesus',
+    --     3
+    -- ),
+    -- (
+    --     'Chintan Shukla',
+    --     'c.shukla',
+    --     'purplelove',
+    --     3
+    -- ),
+    -- (
+    --     'Harshal Dave',
+    --     'harshal.dave',
+    --     'roadyahinbanega',
+    --     3
+    -- );
