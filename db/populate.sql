d7f33ae7bc1d35b2c9dca2de86ebf4e84cf995e0
Gautam <gautam.gv@somaiya.edu>
2022-12-03 14:54:34 +0530

    accounts svvid are now gmail addresses

--- db/populate.sql
+++ db/populate.sql
@@ -35,46 +35,35 @@ INSERT INTO `user` (
 ) VALUES 
     -- Admin User
     (
-        'Administrator',
-        'admin',
-        '$2y$10$XzSladyKL/5lWnM1iTnUzOrmnfzcvTxzuAYKUcOqiCWnsNXRPriN6', -- hellokitty
+        'Gautam Vishwakarma',
+        'gautam.gv@somaiya.edu',
+        '$2y$10$KiwLKW9opfnVcAH5dvFCNOhxxLKoBMET3opGaX02xhpO7dpbryru.', -- gautam123
         1
     ),
     -- Ground Managers
     -- (
-    --     'Abhinandan Goswami',
-    --     'abhi.goswami',
-    --     '$2y$10$W8xk4p8kE7sb8AjYtpXCL.8nunxrBOu.62aBYJlJiTI50OvE3mqTu', -- gogoswami
+    --     'Pramod Dubey',
+    --     'pramod.dubey@somaiya',
+    --     '$2y$10$126FLlbA1k6qFfTQaOJiG.CaaRPqfVPD02sGfeF.sZTPNUVsBdoYq',
     --     2
     -- ),
     (
-        'Pramod Dubey',
-        'pramod.dubey',
-        '$2y$10$126FLlbA1k6qFfTQaOJiG.CaaRPqfVPD02sGfeF.sZTPNUVsBdoYq',
+        'Harshal Dave',
+        'harshal.dave@somaiya.edu',
+        '$2y$10$iKwZq8564YSnCwE50sJovecYQQje8s16WVpXG3EiuqsBFsHU99qaK', -- roadyahinbanega
         2
     ),
     -- Students
     (
-        'Gautam Vishwakarma',
-        'gautam.gv',
-        '$2y$10$KiwLKW9opfnVcAH5dvFCNOhxxLKoBMET3opGaX02xhpO7dpbryru.', -- gautam123
+        'Aditya Pai',
+        'aditya.pai@somaiya.edu',
+        '$2y$10$QK4Lq9.QhHi6lJR3DmC/s.RsCnnNWZ0fVUBOoCFJRUvtlY7OnlpnG', -- hailjesus
         3
     ),
-    -- (
-    --     'Aditya Pai',
-    --     'aditya.pai',
-    --     '$2y$10$QK4Lq9.QhHi6lJR3DmC/s.RsCnnNWZ0fVUBOoCFJRUvtlY7OnlpnG', -- hailjesus
-    --     3
-    -- ),
-    -- (
-    --     'Chintan Shukla',
-    --     'c.shukla',
-    --     '$2y$10$sApKlFh.LbeFeIBroW.dne4sGi0/WNVaO.40z304V9TCKnlpj0P7y', -- purplelove
-    --     3
-    -- ),
     (
-        'Harshal Dave',
-        'harshal.dave',
-        '$2y$10$iKwZq8564YSnCwE50sJovecYQQje8s16WVpXG3EiuqsBFsHU99qaK', -- roadyahinbanega
+        'Chintan Shukla',
+        'c.shukla@somaiya.edu',
+        '$2y$10$sApKlFh.LbeFeIBroW.dne4sGi0/WNVaO.40z304V9TCKnlpj0P7y', -- purplelove
         3
-    );
+    )
+;
